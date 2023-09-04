169488d1ba32a23a508eb9259eff43442ce25e92
Eduardo Hernandez <eduardo@chatbot-plus.com>
2023-09-04 17:35:41 +0200

    add new config for bill views in url

--- save_management/views.py
+++ save_management/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .models import Bill
+from .serializers import BillSerializer
+
+from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+# First tested view get all bills.
+@api_view(['GET'])
+def get_bills(request):
+    notes = Bill.objects.all().order_by('-created')
+    serializer = BillSerializer(notes, many=True)
+    return Response(serializer.data)
