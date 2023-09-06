946748c92d36d66b88dcb0e4b9dd3510b382a5e3
Eduardo Hernandez <eduardo@chatbot-plus.com>
2023-09-06 20:45:10 +0200

    get all bills views with their respective transactions and user

--- save_management/serializers.py
+++ save_management/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Bill
+from .models import Bill, Transaction, ExtendedUser, Category
 
 
 class BillSerializer(ModelSerializer):
     class Meta:
         model = Bill
+        fields = '__all__'
+
+class TransactionSerializer(ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+class ExtendedUserSerialiazer(ModelSerializer):
+    class Meta:
+        model = ExtendedUser
+        fields = '__all__'
+
+class CategorySerializer(ModelSerializer):
+    class Meta:
+        model = Category
         fields = '__all__'
--- save_management/urls.py
+++ save_management/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path('', views.home, name='home'),
     path('all-bills', views.get_bills, name='bills'),
 ]
